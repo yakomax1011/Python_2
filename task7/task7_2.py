343432af8d75bd09cf324a39417da7703c9f79ce
Yakomax <yakomax@system-pro.net>
2021-02-12 00:23:29 +0300

    task7

---
+++ task7/task7_2.py
@@ -0,0 +1,46 @@
+#  Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+#  заданный случайными числами на промежутке [0; 50).
+#  Выведите на экран исходный и отсортированный массивы.
+
+
+import random
+from random import randrange
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
+
+
+SIZE = 10
+arr = [round(random.random() * 50, 2) for i in range(SIZE)]
+print(arr)
+
+
+print(merge_sort(arr))
